4559d6bc3385754470caa6d6cf4fd09194ee756e
luis-valverde120 <luis.valverde@upec.edu.ec>
2026-05-28 00:32:40 -0500

    chore: add functionality for get a current path of user if this no specified a path where need a create'

--- src/generators.rs
+++ src/generators.rs
@@ -2,15 +2,13 @@ use std::path::Path;
 use std::fs;
 use std::io;
 
-mod generators;
-
 fn is_valid_path(path_project: &str) -> bool {
     Path::new(path_project).exists()
 }
 
-pub fn create_folders_structure(path_project: Option<&str>) {
+fn create_folders_structure(path_project: Option<&str>) {
     if let Some(p) = path_project {
-        println!("We have path!");
+        println!("we can create a project") 
     } else {
         println!("We donnot have path given for user");
     } 
@@ -22,5 +20,5 @@ pub fn create_project(name_project: &str) {
         return;
     }
 
-    fs::
+    println!("ok");
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@
  * This is a cli to handle structs and folder architecture a preconfigure all for proyect
  */
 use clap::{Parser, Subcommand, ValueEnum};
+use dialoguer::Confirm;
+use std::env;
+
+mod generators;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -56,17 +60,37 @@ fn main() {
         Commands::New { name, path, framework, arch, lang } => {
             println!("Creating new project: {}", name);
 
-            if let Some(fw) = framework {
-                println!("Selected framework: {:?}", fw);
-            }
+            match path {
+                None => {
 
-            if let Some(a) = arch {
-                println!("Selected architecture: {:?}", a);
-            }
+                    let mut value_of_current_path: String = String::from("");
 
-            if let Some(l) = lang {
-                println!("selected language: {:?}", l);
+                    if let Ok(current_path) = env::current_dir() {
+                        value_of_current_path = current_path.display().to_string();
+                    };
+
+                    let mut string_folder_project = String::from("You create the project in ");
+
+                    string_folder_project.push_str(&value_of_current_path);
+                    string_folder_project.push_str("\nContinue?");
+
+                    let confirmation_path = Confirm::new()
+                        .with_prompt(string_folder_project)
+                        .default(true)
+                        .interact();
+
+                    match confirmation_path {
+                        Err(e) => println!("Error {}", e),
+                        Ok(confirmation) => {
+                            println!("{}", confirmation);
+                        },
+                    };
+                },
+                Some(path) => println!("path: {}", path),
             }
+            
+            generators::create_project(name);
+            
         }
     }
 }
