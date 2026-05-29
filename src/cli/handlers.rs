9f2f7778b72ff2b4457bf81ae86a2f2e343406b4
luis-valverde120 <luis.valverde@upec.edu.ec>
2026-05-29 00:08:58 -0500

    feat!: add functionaliti for handle the path, validations and if the user no put a especific path this ask if the user want to use the current path or no if the user select no this need the path for user

--- src/cli/handlers.rs
+++ src/cli/handlers.rs
@@ -1,7 +1,9 @@
 use crate::cli::args::{Cli, Commands};
+use dialoguer::{Confirm, Input};
 use std::process;
 use std::path::Path;
 use std::env;
+use std::fs;
 
 /*
  * This function valid if a name has spaces or special characters
@@ -25,18 +27,76 @@ fn is_valid_path(path_project: &str) -> bool {
         return false;
     }
 
-    Path::new(path_project).exist()
+    Path::new(path_project).exists()
 }
 
-fn handler_path() {
+fn set_path() -> String {
+    let new_path: String = Input::new()
+        .with_prompt("Specify the path: ")
+        .validate_with(|input: &String| -> Result<(), &str> {
+            if input.trim().is_empty() {
+                return Err("Path is void");
+            }
 
+            if is_valid_path(input) {
+                Ok(())
+            } else {
+                Err("Path is invalid")
+            }
+        })
+        .interact()
+        .unwrap();
+
+        return new_path;
+}
+
+/**
+ * This is a funciton used to create a path with the name given for the user
+ */
+fn handler_path(path: Option<&str>, name: &str) -> Result<String, String> {
+
+    // validate if exist a path if not exist return current path
+    let selected_path = match path {
+        Some(path) => path,
+        None => {
+            &match env::current_dir() {
+                Ok(path) => {
+                    let confirmation = Confirm::new()
+                        .with_prompt(format!("The project is create in {}\nDo you want continue?", path.display().to_string()))
+                        .default(true)
+                        .interact()
+                        .unwrap();
+
+                    if confirmation {
+                        path.display().to_string()
+                    } else {
+                        set_path() // return a path
+                    }
+                },
+                Err(e) => {
+                    println!("We have a error {}", e);
+                    process::exit(1);
+                }
+            }
+        },
+    };
+
+    let new_path = Path::new(selected_path).join(name);
+
+    match fs::create_dir(&new_path) {
+        Ok(_) => {
+            Ok(format!("Create a new directory sucessfully"))
+        },
+        Err(e) => {
+            Err(format!("An Error has ocurrent {}", e))
+        }
+    }
 }
 
 /*
  * This handler the command the flags that use and determine what flags is missing
  */
 pub fn handler_command(cli: Cli) -> Result<(), String> {
-    let mut current_path = String::from(""); 
 
     match &cli.command {
         Commands::New { name, path, framework, arch, lang } => {
@@ -48,21 +108,13 @@ pub fn handler_command(cli: Cli) -> Result<(), String> {
 
             println!("Create a new project {}", name);
 
-
-            match path {
-                Some(path) => {
-                    is_valid_path(&path);
-
+            match handler_path(path.as_deref(), &name) {
+                Ok(result) => {
+                    println!("{}", result);
                 },
-                None => {
-                    let current_path = env::current_dir();
-                    if let Err(e) = current_path {
-                        println!("We have a error using the current dir \n {}", env::current_dir().display());
-                        process::exit(1);
-                    }        
-                    
-
-                },
+                Err(e) => {
+                    println!("Error {}", e);    
+                }
             }
        } 
    } 
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 /*
  * This is a cli to handle structs and folder architecture a preconfigure all for proyect
  */
-use clap::{Parser, Subcommand, ValueEnum};
-use dialoguer::Confirm;
-use std::env;
+use clap::Parser;
 use maker::cli::handlers;
 use maker::cli::args::Cli;
 
@@ -11,45 +9,5 @@ use maker::cli::args::Cli;
 fn main() {
     let cli = Cli::parse();
 
-    handlers::handler_command(cli);
-
-    /*
-    match &cli.command {
-        Commands::New { name, path, framework, arch, lang } => {
-            println!("Creating new project: {}", name);
-
-            match path {
-                None => {
-
-                    let mut value_of_current_path: String = String::from("");
-
-                    if let Ok(current_path) = env::current_dir() {
-                        value_of_current_path = current_path.display().to_string();
-                    };
-
-                    let mut string_folder_project = String::from("You create the project in ");
-
-                    string_folder_project.push_str(&value_of_current_path);
-                    string_folder_project.push_str("\nContinue?");
-
-                    let confirmation_path = Confirm::new()
-                        .with_prompt(string_folder_project)
-                        .default(true)
-                        .interact();
-
-                    match confirmation_path {
-                        Err(e) => println!("Error {}", e),
-                        Ok(confirmation) => {
-                            println!("{}", confirmation);
-                        },
-                    };
-                },
-                Some(path) => println!("path: {}", path),
-            }
-            
-            generators::create_project(name);
-            
-        }
-    }
-    */
+    let _ = handlers::handler_command(cli);
 }
